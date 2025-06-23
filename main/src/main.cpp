bb0819b74ec194fd740628bd25c5ecc09dab32a6
lechrisodile <christansieukam@gmail.com>
2025-06-23 10:47:02 +0200

    initialisation

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -2,14 +2,24 @@
 
 // put function declarations here:
 int myFunction(int, int);
+int ledPin = 13; // Define the pin number for the LED
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200); // Initialize Serial communication at 115200 baud rate
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+
+  // Call the function
+  int result = myFunction(5, 10);
+  Serial.println(result); // Print the result to the Serial Monitor
+
 }
 
 // put function definitions here:
